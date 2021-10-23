07cdd1a32d95c2b2e575de89f1823fa8afce0bd6
azizkhuja <azeez@Azeezs-MacBook-Pro.local>
2021-10-23 22:48:08 +0200

    videos file working

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -71,7 +71,15 @@ export const Results = () => {
                 </div>
             )
         case '/videos':
-            return "VIDEOS";
+            return (
+                <div className="flex flex-wrap">
+                    {results.map((video, index) => (
+                        <div key={index} className="p-2">
+                            <ReactPlayer url={video.additional_links?.[0].href} controls width="355px" height="200px" />
+                        </div>
+                    ))}
+                </div>
+            )
         default:
             return "Error";
     }
